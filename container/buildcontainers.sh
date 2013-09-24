2248a579a61b7896c985d7fd8f9139f771d7eb19
Matthew Miller <mattdm@mattdm.org>
2013-09-24 19:56:53 -0400

    here's an excitingly hacky build script. you shouldn't really use this. i just dont want to lose it if i lose my laptop.

---
+++ container/buildcontainers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for size in small medium; do
+for ver in 19 20; do
+  appliance-creator -c container-$size-$ver.ks -d -v -t /tmp \
+     -o /tmp/f$ver$size --name "fedora-$ver-$size" --release $ver \
+     --format=qcow2 && 
+  virt-tar-out -a /tmp/f$ver$size/fedora-$ver-$size/fedora-$ver-$size-sda.qcow2 / - |
+  import - fedora$ver-$size
+done
+done
