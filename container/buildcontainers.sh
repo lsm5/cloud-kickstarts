91a6d05b1c450b9c09e459899b1c329393bb2032
Matthew Miller <mattdm@mattdm.org>
2013-10-14 18:29:33 -0400

    tag containers with my repo, for now. again, this is a kludgey script not meant for real use

--- container/buildcontainers.sh
+++ container/buildcontainers.sh
@@ -1,10 +1,16 @@
 #!/bin/bash -x
+repoowner=mattdm
 for size in small medium; do
 for ver in 19 20; do
+  if [[ "$size" == 'medium' ]]; then
+     repo=$repoowner/fedora
+  else
+     repo=$repoowner/fedora-$size
+  fi
   appliance-creator -c container-$size-$ver.ks -d -v -t /tmp \
      -o /tmp/f$ver$size --name "fedora-$ver-$size" --release $ver \
      --format=qcow2 && 
   virt-tar-out -a /tmp/f$ver$size/fedora-$ver-$size/fedora-$ver-$size-sda.qcow2 / - |
-  docker import - fedora$ver-$size
+  docker import - $repo f$ver
 done
 done
