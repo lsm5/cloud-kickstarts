07d0865ec184e8abaabcdbf541633695478d6d9f
Lokesh Mandvekar <lsm5@redhat.com>
2013-12-17 16:06:48 -0600

    rawhide repos updated
    Signed-off-by: Lokesh Mandvekar <lsm5@redhat.com>

---
+++ container/imagebuilder.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+repoowner=lsm5
+unset LIBGUESTFS_BACKEND
+repo=$repoowner/fedora-imagebuilder
+appliance-creator -c container-imagebuilder.ks -d -v -t /tmp \
+    -o /tmp/fimagebuilder --name "fedora-imagebuilder" --release 20 \
+    --format=qcow2
+virt-tar-out -a \
+    /tmp/fimagebuilder/fedora-imagebuilder-sda.qcow2 / - | \
+    gzip --best > /tmp/fedora-imagebuilder.tar.gz
+export LIBGUESTFS_BACKEND=direct
+cat /tmp/fedora-imagebuilder.tar.gz | docker import - $repo
