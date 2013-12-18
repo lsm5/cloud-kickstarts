2ef8fd845258ecf4cc1df71f4ddc1ebb352b12f2
Lokesh Mandvekar <lsm5@redhat.com>
2013-12-18 13:23:08 -0600

    change LIBGUESTFS_BACKEND export order
    Signed-off-by: Lokesh Mandvekar <lsm5@redhat.com>

--- container/imagebuilder.sh
+++ container/imagebuilder.sh
@@ -5,7 +5,8 @@ unset LIBGUESTFS_BACKEND
 appliance-creator -c container-imagebuilder.ks -d -v -t /home/$repoowner \
     -o /home/$repoowner/fimagebuilder --name "fedora-imagebuilder" --release 20 \
     --format=qcow2
-LIBGUESTFS_BACKEND=direct virt-tar-out -a \
+virt-tar-out -a \
     /home/$repoowner/fimagebuilder/fedora-imagebuilder/fedora-imagebuilder-sda.qcow2 / - | \
     gzip --best > /home/$repoowner/fedora-imagebuilder.tar.gz
+export LIBGUESTFS_BACKEND=direct
 cat /home/$repoowner/fedora-imagebuilder.tar.gz | docker import - $repo
